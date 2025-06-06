9504374dc807c93b9e56627dbe98bba08c89f38c
Shashank <mshashank02@gmail.com>
2025-06-06 15:53:16 -0700

    added shadowhand

--- dreamerv3/main.py
+++ dreamerv3/main.py
@@ -217,6 +217,7 @@ def make_env(config, index, **overrides):
   ctor = {
       'dummy': 'embodied.envs.dummy:Dummy',
       'gym': 'embodied.envs.from_gym:FromGym',
+      'gymnasium': 'embodied.envs.from_gymnasium:FromGymnasium',
       'dm': 'embodied.envs.from_dmenv:FromDM',
       'crafter': 'embodied.envs.crafter:Crafter',
       'dmc': 'embodied.envs.dmc:DMC',
@@ -244,6 +245,8 @@ def make_env(config, index, **overrides):
     kwargs['logdir'] = elements.Path(config.logdir) / f'env{index}'
   env = ctor(task, **kwargs)
   return wrap_env(env, config)
+
+
 
 
 def wrap_env(env, config):
